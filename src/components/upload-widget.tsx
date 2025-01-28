265d1d39aba60399de248d47da4a6d03e26c2150
basilioarth <basilio.arth@gmail.com>
2025-01-28 19:51:14 -0300

    feat: Borda gradiente no botão

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -6,6 +6,7 @@ import { motion, useCycle } from "motion/react";
 import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
 
  export function UploadWidget() {
+    const isThereAnyPendingUpload = true
     const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true) 
     /**
      * O elemento da esquerda é o valor padrão. O valor da direita é o valor para o qual o estado 
@@ -14,9 +15,14 @@ import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
      */
 
     return (
-        <Collapsible.Root onOpenChange={() => toggleWidgetOpen()}>
+        <Collapsible.Root onOpenChange={() => toggleWidgetOpen()} asChild>
+            {/**
+             * O uso do asChild faz com que o Collapsible.Root não crie uma nova div, mas sim
+             * mantenha a div abaixo (nesse caso a motion.div) como sendo a própria Collapsible.Root
+             */}
             <motion.div
-                className="bg-zinc-900 overflow-hidden max-w-[360px] rounded-xl shadow-shape"
+                data-progress={isThereAnyPendingUpload}
+                className="bg-zinc-900 overflow-hidden max-w-[360px] rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,theme(colors.zinc.900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.zinc.700/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.zinc.600/.48))_border-box]"
                 animate={isWidgetOpen ? "open" : "closed"}
                 variants={{
                     closed: {
