3f6c9900346a76ad39affa5831e6123a41b46761
basilioarth <basilio.arth@gmail.com>
2025-02-02 19:48:37 -0300

    feat: Compressão de imagens com canvas

---
+++ src/utils/compress-image.ts
@@ -0,0 +1,101 @@
+interface CompressImageParams {
+    file: File,
+    maxWidth?: number
+    maxHeight?: number
+    quality?: number
+}
+
+function convertToWebp(filename: string): string {
+    const lastDotIndex = filename.lastIndexOf('.')
+
+    if (lastDotIndex === -1) {
+        return `${filename}.webp`;
+    }
+
+    return `${filename.substring(0, lastDotIndex)}.webp`
+}
+
+export function compressImage({
+    file,
+    maxWidth = Number.POSITIVE_INFINITY,
+    maxHeight = Number.POSITIVE_INFINITY,
+    quality = 1
+}: CompressImageParams) {
+    const allowedFileTypes = [
+        'image/jpg',
+        'image/jpeg',
+        'image/png',
+        'image/webp',
+    ]
+
+    if (!allowedFileTypes.includes(file.type)) {
+        throw new Error('Image format not supported.')
+    }
+
+    return new Promise<File>((resolve, reject) => {
+        const reader = new FileReader()
+
+        reader.onload = event => {
+            const compressed = new Image()
+
+            compressed.onload = () => {
+                const canvas = document.createElement('canvas')
+
+                let width = compressed.width // Largura original da imagem
+                let height = compressed.height // Altura original da imagem
+
+                if (width > height) { // Imagem mais larga do que alta
+                    if (width > maxWidth) {
+                        width = maxWidth // Limitando a largura à largura máxima
+                        height *= maxWidth / width // A altura está diminuindo proporcionalmente à largura máxima
+                    }
+                } else { // Imagem mais altra do que larga
+                    if (height > maxHeight) {
+                        height = maxHeight // Limitando a altura à altura máxima
+                        width *= maxHeight / height // A largura está diminuindo proporcionalmente à alktura máxima
+                    }
+                }
+
+                canvas.width = width
+                canvas.height = height
+
+                const context = canvas.getContext('2d')
+
+                if (!context) {
+                    reject(new Error('Failed to get canvas context.'))
+                    return
+                }
+
+                context.drawImage(compressed, 0, 0, width, height)
+
+                canvas.toBlob(
+                    blob => {
+                        if (!blob) {
+                            reject(new Error('Failed to compress image.'))
+                            return
+                        }
+
+                        const compressedFile = new File(
+                            [blob],
+                            convertToWebp(file.name),
+                            {
+                                type: 'image/webp',
+                                lastModified: Date.now()
+                            }
+                        )
+
+                        resolve(compressedFile)
+                    },
+                    'image/webp',
+                    quality,
+                )
+                /**
+                 * Blob é uma representação binária da imagem
+                 */
+            }
+
+            compressed.src = event.target?.result as string
+        }
+        reader.readAsDataURL(file)
+    })
+}
